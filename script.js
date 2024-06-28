a589817a836deede6398c488ba7df12b3d6c532f
joksdz <joksdzjojo2007@gmail.com>
2024-06-28 21:19:03 +0100

    colors

--- script.js
+++ script.js
@@ -4,8 +4,9 @@ function draw() {
     const ctx = canvas.getContext("2d");
     ctx.beginPath();
     ctx.arc(75, 75, 50, 0, 2 * Math.PI);
-    ctx.stroke();
+    ctx.fillStyle = "#d53600";
     ctx.fill();
+    ctx.stroke();
   }
 }
 
