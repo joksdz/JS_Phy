e57e157936fca2adc50b2a89f0f6b1ee799e7203
joksdz <joksdzjojo2007@gmail.com>
2024-06-28 23:41:06 +0100

    small changes

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-function draw() {
+function balls(x,y,r,a1,a2) {
   const canvas = document.getElementById("canvas");
   if (canvas.getContext) {
     const ctx = canvas.getContext("2d");
     ctx.beginPath();
-    ctx.arc(75, 75, 50, 0, 2 * Math.PI);
+    ctx.arc(x,y,r, a1, a2);
     ctx.fillStyle = "#d53600";
     ctx.fill();
     ctx.stroke();
   }
 }
 
-draw();
+balls(200,200,45,0,2*Math.PI);
+
