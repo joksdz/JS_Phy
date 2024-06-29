fa7acf80ab858a3334c54b80ad70f08af20a5d56
joksdz <joksdzjojo2007@gmail.com>
2024-06-29 22:13:42 +0100

    OOP typa shit+more balls (nice)

--- script.js
+++ script.js
@@ -1,29 +1,33 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
 let left,right, up , down
-
-var x = 100
-var y = 100
-var r = 45
-function balls(x,y,r) {
+const ballz = []
+class Ball {
+constructor(x, y, r) {
+  this.x = x;
+  this.y = y;
+  this.r = r;
+  this.player = false
+    ballz.push(this)
+  }
+  balls() {
       ctx.beginPath();
-    ctx.arc(x,y,r,0,2*Math.PI);
+    ctx.arc(this.x,this.y,this.r,0,2*Math.PI);
+    ctx.stroke();
     ctx.fillStyle = "#d53600";
     ctx.fill();
-    ctx.stroke();
   }
+}
+
+function control(b){
+
 canvas.addEventListener("keydown", function (e) {
 if (e.keyCode == 37){left = true}
 if (e.keyCode == 38){up = true}
 if (e.keyCode == 39){right = true}
 if (e.keyCode == 40){down = true}
 })
-function move() {
-  if (left){x--}
-  if (right){x++}
-  if (up){y--}
-  if (down){y++}
-}
+
 canvas.addEventListener("keyup", function (e) {
 if (e.keyCode == 37){left = false}
 if (e.keyCode == 38){up = false}
@@ -31,14 +35,28 @@ if (e.keyCode == 39){right = false}
 if (e.keyCode == 40){down = false}
 })
 
+  if (left){b.x--}
+  if (right){b.x++}
+  if (up){b.y--}
+  if (down){b.y++}
+
+}
+
  function repeat(){
    ctx.clearRect(0, 0, canvas.width, canvas.height);
-  move();
-  balls(x , y, r);
+ ballz.forEach((b)=>{b.balls()
+ control(b)
+ })
 requestAnimationFrame(repeat) 
 
 }
+ let b = new Ball(200,200,50)
+b.player = true
+
+
+
+
+
+
+
 requestAnimationFrame(repeat)
-
-
-
