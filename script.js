c05bda48208a5a355429a9d6d48dd6806e4ba8aa
joksdz <joksdzjojo2007@gmail.com>
2024-06-29 20:36:27 +0100

    we got movement

--- script.js
+++ script.js
@@ -1,20 +1,44 @@
 const canvas = document.getElementById("canvas");
 const ctx = canvas.getContext("2d");
+let left,right, up , down
 
-function balls(x,y,r,a1,a2) {
+var x = 100
+var y = 100
+var r = 45
+function balls(x,y,r) {
       ctx.beginPath();
-    ctx.arc(x,y,r, a1, a2);
+    ctx.arc(x,y,r,0,2*Math.PI);
     ctx.fillStyle = "#d53600";
     ctx.fill();
     ctx.stroke();
   }
 canvas.addEventListener("keydown", function (e) {
-  if (e.keyCode === 69) {
-    balls(200,100,45,0,2*Math.PI);
-  }
+if (e.keyCode == 37){left = true}
+if (e.keyCode == 38){up = true}
+if (e.keyCode == 39){right = true}
+if (e.keyCode == 40){down = true}
+})
+function move() {
+  if (left){x--}
+  if (right){x++}
+  if (up){y--}
+  if (down){y++}
+}
+canvas.addEventListener("keyup", function (e) {
+if (e.keyCode == 37){left = false}
+if (e.keyCode == 38){up = false}
+if (e.keyCode == 39){right = false}
+if (e.keyCode == 40){down = false}
 })
 
+ function repeat(){
+   ctx.clearRect(0, 0, canvas.width, canvas.height);
+  move();
+  balls(x , y, r);
+requestAnimationFrame(repeat) 
+
+}
+requestAnimationFrame(repeat)
 
 
-balls(200,200,45,0,2*Math.PI);
 
