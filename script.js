644bbed32cda044d358eff0e883520b4afd5633e
joksdz <joksdzjojo2007@gmail.com>
2024-06-29 19:54:24 +0100

    balls

--- script.js
+++ script.js
@@ -1,14 +1,20 @@
+const canvas = document.getElementById("canvas");
+const ctx = canvas.getContext("2d");
+
 function balls(x,y,r,a1,a2) {
-  const canvas = document.getElementById("canvas");
-  if (canvas.getContext) {
-    const ctx = canvas.getContext("2d");
-    ctx.beginPath();
+      ctx.beginPath();
     ctx.arc(x,y,r, a1, a2);
     ctx.fillStyle = "#d53600";
     ctx.fill();
     ctx.stroke();
   }
-}
+canvas.addEventListener("keydown", function (e) {
+  if (e.keyCode === 69) {
+    balls(200,100,45,0,2*Math.PI);
+  }
+})
+
+
 
 balls(200,200,45,0,2*Math.PI);
 
